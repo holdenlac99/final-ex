d47e32de6918465a1dabf9b2dd27495174842631
holdenlac99 <153549368+holdenlac99@users.noreply.github.com>
2023-12-11 11:59:44 -0800

    Update MacroTracker.java

--- SampleWebApp/src/MacroTracker.java
+++ SampleWebApp/src/MacroTracker.java
@@ -1,4 +1,3 @@
-
 @WebServlet("/MacroTracker")
 public class MacroTracker extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -8,16 +7,25 @@ public class MacroTracker extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        handlePostRequest(request, response);
+    }
 
+    private void handlePostRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
 
         String nameOfFood = request.getParameter("nameOfFood");
-        int cal = Integer.parseInt(request.getParameter("cal"));
-        int protein = Integer.parseInt(request.getParameter("protein"));
-        int amount = Integer.parseInt(request.getParameter("amount"));
 
+        try {
+            int cal = Integer.parseInt(request.getParameter("cal"));
+            int protein = Integer.parseInt(request.getParameter("protein"));
+            int carbs = Integer.parseInt(request.getParameter("carbs"));
+            int amount = Integer.parseInt(request.getParameter("amount"));
 
-        response.getWriter().println("Data received: " + nameOfFood + ", Calories: " + cal +
-                ", Protein: " + protein + ", amount: " + amount);
+            response.getWriter().println("Data received: " + nameOfFood + ", Calories: " + cal +
+                    ", Protein: " + protein + ", Carbs: " + carbs + ", Amount: " + amount);
+        } catch (NumberFormatException e) {
+            response.getWriter().println("Invalid input for calories, protein, carbs, or amount.");
+        }
     }
 }
+
