1f784a9e1f043ff046d8699f63cd7098a3044a1e
holdenlac99 <153549368+holdenlac99@users.noreply.github.com>
2023-12-12 10:52:34 -0600

    Update MacroTracker.java

--- SampleWebApp/src/MacroTracker.java
+++ SampleWebApp/src/MacroTracker.java
@@ -1,31 +1,27 @@
-@WebServlet("/MacroTracker")
-public class MacroTracker extends HttpServlet {
+@WebServlet("/SaveDataServlet")
+public class SaveDataServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         handlePostRequest(request, response);
     }
 
     private void handlePostRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        String nameOfFood = request.getParameter("nameOfFood");
+        String foodName = request.getParameter("foodName");
 
         try {
-            int cal = Integer.parseInt(request.getParameter("cal"));
+            int calories = Integer.parseInt(request.getParameter("calories"));
             int protein = Integer.parseInt(request.getParameter("protein"));
             int carbs = Integer.parseInt(request.getParameter("carbs"));
             int amount = Integer.parseInt(request.getParameter("amount"));
 
-            response.getWriter().println("Data received: " + nameOfFood + ", Calories: " + cal +
+            response.getWriter().println("Data received: " + foodName + ", Calories: " + calories +
                     ", Protein: " + protein + ", Carbs: " + carbs + ", Amount: " + amount);
         } catch (NumberFormatException e) {
             response.getWriter().println("Invalid input for calories, protein, carbs, or amount.");
         }
     }
 }
-
